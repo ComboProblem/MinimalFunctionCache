1786bbb7dd3f28faf0d283f3c7065ad92e389c13
ComboProblem <102884863+ComboProblem@users.noreply.github.com>
2026-03-06 15:46:56 -0800

    for testing of container

--- MinimalFunctionCacheGen/cache_gen.sh
+++ MinimalFunctionCacheGen/cache_gen.sh
@@ -2,10 +2,8 @@
 
 # This script is assumed to be ran on the host computer and can send slurm jobs.
 
-
 # Set cache computation parametesr
 NUM_BKPT = 2
-
 # Estimate CPU time required.
 SAMPLE_SIZE = 2 # increasing sample size increases the inital run time which could lenghtly wiht current implementation.
 TIME_PER_BATCH = 60 # mesured in minutes
@@ -14,9 +12,10 @@ MAX_STD = 5 # used in time estimages
 MAX_NUM_ROW = 1000 # in each breakpoint file.
 BACKEND = "pplite"
 
-export BKPTS_PATH = "~/MinimalFunctionCache/Breakpoints/$NUM_BKPT"
 mkdir ~/MinimalFunctionCache/TEMP
 export MFC_TEMP = "~/MinimalFunctionCache/TEMP"
+export BKPTS_PATH = "$MFC_TEMP/Breakpoints/$NUM_BKPT"
+
   # SBATCH --
   # SBATCH --partition=low
   # SBATCH --ntasks=1
@@ -29,21 +28,20 @@ export MFC_TEMP = "~/MinimalFunctionCache/TEMP"
   # load module(s)
   module purge
   module load apptainer
-e
+
   # specify what path(s) to bind inside the apptainer
   export $APPTAINER_BIND=$PWD
-
+# typically home directory is shared over the nextwork
+# file systems are realitive slow
   # check for an apptainer(singularity) image (.sif) and build/pull one if it doesn't exist
   # Assumes that Apptainer.def is in PWD.
 
   apptainer run cgf.sif cache_job_setup.py NUM_BKPT SAMPLE_SIZE TIME_PER_BATCH MAX_STD NUM_ROW BACKEND
 
-# verify
-echo NUM_ROWS: $NUM_ROWS
-echo NUM_JOBS: $NUM_JOBS
-echo ALLOC_TIME_PER_JOB: $ALLOC_TIME_PER_JOB
-  # check new env vars are set
-
-  # add a continue option/flag always yes, always no, ask when finished (default behavior).
-
-  # Here is where we make a loop for excute jobs. We
+# load set up variables
+$MFC_TEMP/temp_job_info
+# check if we should keep goin'
+if [$RUN_COMPUTATION==0]; then
+  echo "Run aborted."
+  exit 0
+fi
--- MinimalFunctionCacheGen/cache_job_setup.py
+++ MinimalFunctionCacheGen/cache_job_setup.py
@@ -7,7 +7,7 @@ from random import sample
 import logging
 
 inital_gen_logger = logging.getLogger(__name__)
-
+inital_gen_logger.setLevel(logging.info)
 # read bash inputs
 k = int(system_args[1]) # for now keep k small, <= 9 from initial testing.
 sample_size = int(system_args[2]) # pick a value between 2 and 100;
@@ -26,8 +26,15 @@ try:
     run_computation_default = str(system_args[8])
 except IndexError:
     run_computation_default = None
+try:
+    logging_on = bool(system_args[9])
+    if not logging_on:
+        inital_gen_logger.setLevel(logging.error)
+except IndexError:
+    pass
 batch_time_default = 10
 
+print(f"{time.time()}: Setting up SLURM job parameters.")
 if run_computation_default is not None:
     if run_computation_default.lower().strip(" ") == "n" or run_computation_default.lower().strip(" ") == "no":
         run_computation = False
@@ -47,6 +54,8 @@ assert(time_per_batch >= batch_time_default)
 assert(sample_size >= 2)
 assert(sample_size <= 100)
 assert(max_number_of_rows >= 1)
+#TODO: check if breakpoints have been written aready and load them if possible.
+
 inital_gen_logger.info("Computing breakpoints...")
 bkpts = BreakpointComplexClassContainer(k)
 
@@ -82,48 +91,56 @@ if number_of_rows > max_number_of_rows:
 else:
     batch_time_less_than_estimated_computation_time = False
 number_of_batches = int(num_bkpts_seqs/number_of_rows) + 1
-inital_gen_logger.info(f"Number of rows per bkpt file:  {number_of_rows}\nNumber of rows per min_fun_rep file: {number_of_rows*k} \nNumber of Batches: {number_of_batches}")
+mkdir ~/MinimalFunctionCache/TEMP
+(f"Number of rows per bkpt file:  {number_of_rows}\nNumber of rows per min_fun_rep file: {number_of_rows*k} \nNumber of Batches: {number_of_batches}")
 inital_gen_logger.info("Inferring batch time estimate based on parameters")
 sample_std =  std(sample_space)
 if overhead_time*60 > sample_std * max_std:
     time_alloc = time_per_batch + overhead_time
 else:
     time_alloc = time_per_batch + (num_rows*sample_std * max_std)/60
-print(f"Allocated time batch: {time_alloc:.2f} minutes.")
-print(f"Allocating {time_alloc * number_of_batches:.2f} minutes of CPU time.")
+inital_gen_logger.info(f"Allocated time batch: {time_alloc:.2f} minutes.")
+inital_gen_logger.info(f"Allocating {time_alloc * number_of_batches:.2f} minutes of CPU time.")
 
 # if not set to default run or stop, ask user to decide if the program should run
 if run_computation is None:
-    print(f"Do you want to continue to run the computation? Y or N?")
+    print(f"{time.time()}: Do you want to continue to run the computation? Y or N?")
     valid_input = False
     while not valid_input:
         cont = input()
         if cont.lower().strip(' ') == "n":
             run_computation = False
             valid_input = True
-            print("Aborting run...")
+            print(f"{time.time()}: Aborting run...")
         elif cont.lower().strip(' ') == "y":
             run_computation = True
             valid_input = True
-            print("Run approved. Carrying on with computation.")
+            print(f"{time.time()}: Run approved. Carrying on with computation.")
         else:
-            print("Please input either Y or N to continue or not")
+            print(f"{time.time()}: Please input either Y or N to continue or not.")
     # prompt user to set run_computation, if not already set
 
 
-#os.putenv(RUN_JOBS, run_computation) # some type of bool  do
 if run_computation:
     # write breakpoints
+    # enviroment vars are attached to process
     os.chdir(os.getenv("BKPTS_PATH"))
     bkpts.write_data(max_rows=max_number_of_rows)
     os.chdir(os.getenv("MFC_TEMP"))
     with open("temp_job_info.sh", "w") as run_vars_file:
-        run_vars_file.write(f"NUM_ROWS={number_of_rows}\n")
-        run_vars_file.write(f"NUM_JOBS={number_of_batches}\n")
-        run_vars_file.write(f"ALLOC_TIME_PER_JOB={time_alloc}")
+        run_vars_file.write("#!/bin/bash\n")
+        run_vars_file.write(f"export RUN_COMPUTATION=1\n")
+        run_vars_file.write(f"export NUM_ROWS={number_of_rows}\n")
+        run_vars_file.write(f"export NUM_JOBS={number_of_batches}\n")
+        run_vars_file.write(f"export ALLOC_TIME_PER_JOB={time_alloc}")
     # os.close("temp_job_info.sh")
     # Set ENV variables for this run
     # record file writing location and add to path.
     # os.putenv("NUM_ROWS", str(number_of_rows))
     # os.putenv("NUM_JOBS", str(number_of_batches))
     # os.putenv("ALLOC_TIME_PER_JOB", str(time_alloc))
+else:
+    os.chdir(os.getenv("MFC_TEMP"))
+    with open("temp_job_info.sh", "w") as run_vars_file:
+        run_vars_file.write("#!/bin/bash\n")
+        run_vars_file.write(f"export RUN_COMPUTATION=0")
