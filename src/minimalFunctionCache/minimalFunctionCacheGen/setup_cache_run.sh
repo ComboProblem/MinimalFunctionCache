6847563642a5b701c6ebfd50332ea7aee40560b8
Acadia Larsen <acadia.larsen@gmail.com>
2026-04-02 11:53:04 -0700

    T

--- src/minimalFunctionCache/minimalFunctionCacheGen/setup_cache_run.sh
+++ src/minimalFunctionCache/minimalFunctionCacheGen/setup_cache_run.sh
@@ -5,6 +5,6 @@ echo "Setting up run."
 source ~/MinimalFunctionCache/src/minimalFunctionCache/minimalFunctionCacheGen/cache_gen_run_parameters.sh
 
 module load apptainer
-apptainer run cgf.sif python3/MinimalFunctionCache/src/minimalFunctionCache/minimalFunctionCacheGen/cache_job_setup.py $NUM_BKPT $SAMPLE_SIZE $TIME_PER_BATCH $MAX_NUM_ROW $MAX_STD $BACKEND
+apptainer run cgf.sif python3 ~/MinimalFunctionCache/src/minimalFunctionCache/minimalFunctionCacheGen/cache_job_setup.py $NUM_BKPT $SAMPLE_SIZE $TIME_PER_BATCH $MAX_NUM_ROW $MAX_STD $BACKEND
 
 ls ~/MinimalFunctionCache/TEMP
