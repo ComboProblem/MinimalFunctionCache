ac8dc8639739f42c81b88abd6e5537d9613f2450
Acadia Larsen <acadia.larsen@gmail.com>
2026-04-09 08:07:10 -0700

    T

--- src/minimalFunctionCache/minimalFunctionCacheGen/cluster_cache_gen.sh
+++ src/minimalFunctionCache/minimalFunctionCacheGen/cluster_cache_gen.sh
@@ -54,19 +54,57 @@ fi
 }
 
 main(){
+# get parameters and load temp
+source ./MinimalFunctionCache/src/minimalFunctionCache/minimalFunctionCacheGen/cache_gen_run_parameters.sh
+echo "Parameters loaded; running cache generation."
+mkdir $MFC_TEMP
 
-get_parameters_and_load_temp
 echo "$NUM_BKPT_LOWER_BOUND,  $NUM_BKPT_UPPER_BOUND"
 
 for ((NUM_BKPT = $NUM_BKPT_LOWER_BOUND; NUM_BKPT <= $NUM_BKPT_UPPER_BOUND; NUM_BKPT++))
 do
 echo "Starting run for $NUM_BKPT."
 
-setup_file_system
+# setup file system
+if [ ! -d "$BKPT_PATH_BASE/$NUM_BKPT" ]; then
+    mkdir -p $BKPT_PATH_BASE/$NUM_BKPT
+else
+    echo "path: $BKPT_PATH_BASE/$NUM_BKPT exists; proceed with caution."
+fi
+if [ ! -d "$REP_ELEM_PATH_BASE/$NUM_BKPT" ]; then
+    mkdir -p $REP_ELEM_PATH_BASE/$NUM_BKPT
+else
+    echo "path: $REP_ELEM_PATH_BASE/$NUM_BKPT exists; proceed with caution."
+fi
 
-setup_apptainer
+#setup_apptainer
+echo "Checking for apptainer."
+module load apptainer
+if [ ! -f "$MFC_TARGET/cgf.sif" ]; then
+  echo "Building apptainer"
+  apptainer build "$MFC_TARGET/cgf.sif" "$APPTAINER_DEF_PATH"
+fi
 
-setup_cache_run
+# setup_cache_run
+echo "Running inital setup."
+chmod +x ./MinimalFunctionCache/src/minimalFunctionCache/minimalFunctionCacheGen/setup_cache_run.sh
+if [ "$FLAGS" == *c* ]; then 
+sbatch --partition=$PARTITION --account=$CLUSTER_ACCOUNT --ntasks=1 --cpus-per-task=1 --time=$INITAL_TIME:00  ./MinimalFunctionCache/src/minimalFunctionCache/minimalFunctionCacheGen/setup_cache_run.sh $NUM_BKPT
+else
+ ~/MinimalFunctionCache/src/minimalFunctionCache/minimalFunctionCacheGen/setup_cache_run.sh $NUM_BKPT
+fi
+# Now load job info
+source $MFC_TEMP/temp_job_info_for_$NUM_BKPT.sh
+
+# check if we should keep goin'
+if [ $RUN_COMPUTATION == '0' ]; then
+  echo "Run aborted."
+  echo "Cleaning temp files."
+  rm -rf $MFC_TEMP
+  exit 0
+else
+  echo "Run Approved - Computing $NUM_BKPT."
+fi
 
 echo "Run setup complete $NUM_BKPT. Dispatching jobs."
 chmod +x ~/MinimalFunctionCache/src/minimalFunctionCache/minimalFunctionCacheGen/job_function_runner.sh
